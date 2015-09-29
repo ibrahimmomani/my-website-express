4e2568e2025b57b76eecdecc12116fdb6119f880
Ibrahim Momani <ibrahimmomani@gmail.com>
2015-09-29 06:59:00 +0000

    Cache-control Header added.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.setHeader('Cache-Control', 'public, max-age=86400');
   res.render('index');
 });
 
